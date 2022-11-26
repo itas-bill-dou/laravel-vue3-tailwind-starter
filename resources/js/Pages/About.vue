8eee57e7c503803903b1d3e613d52209874bc8c6
itas-bill-dou <90282490+itas-bill-dou@users.noreply.github.com>
2022-11-25 18:11:49 -0800

    Delete resources/js/Pages directory

--- resources/js/Pages/About.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import img from '@src/images/about.webp'
-</script>
-<template>
-    <div class="flex flex-col items-center">
-        <img class="w-1/2" :src="img" alt="happy-guy" />
-        <span class="text-3xl font-semibold">About</span>
-    </div>
-</template>
